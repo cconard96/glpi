1886b2b42e8cd09b386c787c00b01320e3a82c75
cconard96 <cconard96@gmail.com>
2021-12-29 11:50:17 -0500

    Experiment with front file display tests

---
+++ tests/cypress/integration/item_form_pages_display.spec.disabled.js
@@ -0,0 +1,99 @@
+describe('Item form pages display', () => {
+   const testPages = (all_pages, type, category) => {
+      Object.entries(all_pages).forEach(([front_file, props]) => {
+         if (props.type === type && props.category && props.category === category) {
+            cy.visit('front/' + front_file);
+            cy.get('form').should('exist');
+            cy.get('form button[name="add"]').should('exist');
+            cy.expectNoDebugErrors();
+         }
+      });
+   };
+   it('Item form pages displays without errors: Assets', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'assets');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Components', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'components');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Network', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'network');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Inventory', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'inventory');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Assistance', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'assistance');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Management', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'management');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Tools', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'tools');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Setup', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'setup');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Knowledgebase', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'knowledgebase');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Rules', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'rules');
+         });
+      });
+   });
+
+   it('Item form pages displays without errors: Notifications', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'item_form', 'notifications');
+         });
+      });
+   });
+});
---
+++ tests/cypress/integration/search_pages_display.spec.js
@@ -0,0 +1,99 @@
+describe('Search pages display', () => {
+   const testPages = (all_pages, type, category) => {
+      Object.entries(all_pages).forEach(([front_file, props]) => {
+         if (props.type === type && props.category && props.category === category) {
+            cy.visit('front/' + front_file);
+            cy.get('.search-form-container').should('exist');
+            cy.get('table.search-results').should('exist');
+            cy.expectNoDebugErrors();
+         }
+      });
+   };
+   it('Search pages displays without errors: Assets', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'assets');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Components', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'components');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Network', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'network');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Inventory', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'inventory');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Assistance', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'assistance');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Management', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'management');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Tools', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'tools');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Setup', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'setup');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Knowledgebase', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'knowledgebase');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Rules', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'rules');
+         });
+      });
+   });
+
+   it('Search pages displays without errors: Notifications', () => {
+      cy.loginByID('super-admin').then(() => {
+         cy.fixture('front_files').then((front_files) => {
+            testPages(front_files, 'search', 'notifications');
+         });
+      });
+   });
+});
