4c8ca6375eac09f460cc7585bc546177baa79b72
stonebuzz <skita@teclib.com>
2023-03-08 08:23:14 +0100

    feat(core): webhooks implementation

--- src/Html.php
+++ src/Html.php
@@ -1345,7 +1345,7 @@ HTML;
             'config' => [
                 'title' => __('Setup'),
                 'types' => [
-                    'CommonDropdown', 'CommonDevice', 'Notification',
+                    'CommonDropdown', 'CommonDevice', 'Notification', 'Webhook',
                     'SLM', 'Config', 'FieldUnicity', 'CronTask', 'Auth',
                     'MailCollector', 'Link', 'Plugin', 'OAuthClient'
                 ],
