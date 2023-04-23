6275cc92a95a725c31978acc79c2495477bded1d
Curtis Conard <cconard96@gmail.com>
2023-04-23 11:51:07 -0400

    only go to index

--- tests/cypress/e2e/test.cy.js
+++ tests/cypress/e2e/test.cy.js
@@ -1,8 +1,5 @@
 describe('Issue 13433', () => {
     it('load home page', () => {
-        cy.loginByID('super-admin').then(() => {
-            // Test navigation menu exists
-            cy.get('#navbar-menu').should('exist');
-        });
+        cy.visit('/');
     });
 });
