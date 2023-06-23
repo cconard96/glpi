c5c12db505fd26a1907e8bf06732d7a3dc9a68ef
Curtis Conard <cconard96@gmail.com>
2023-06-23 06:49:48 -0400

    simplify

--- tests/e2e/AbstractE2ETest.php
+++ tests/e2e/AbstractE2ETest.php
@@ -35,6 +35,7 @@
 
 namespace Glpi\Tests\E2E;
 
+use Facebook\WebDriver\Exception\TimeoutException;
 use Symfony\Component\Panther\Client;
 use Symfony\Component\Panther\PantherTestCase;
 
@@ -42,6 +43,8 @@ abstract class AbstractE2ETest extends PantherTestCase
 {
     /** @var Client */
     private $client;
+
+    private const DEFAULT_TIMEOUT = 1;
 
     protected function getClient(): Client
     {
@@ -51,21 +54,87 @@ abstract class AbstractE2ETest extends PantherTestCase
         return $this->client;
     }
 
+    protected function visit(string $url, array $options = [])
+    {
+        $method = $options['method'] ?? 'GET';
+        $this->getClient()->request($method, $url);
+    }
+
+    protected function title(): string
+    {
+        return $this->getClient()->getTitle();
+    }
+
+    protected function url(): string
+    {
+        return $this->getClient()->getCurrentURL();
+    }
+
+    protected function get(string $selector, array $options = [])
+    {
+        $this->expectToExist($selector, $options);
+        return $this->getClient()->getCrawler()->filter($selector);
+    }
+
+    protected function setValue(string $selector, mixed $value, array $options = [])
+    {
+        $this->expectToExist($selector, $options);
+        $val = is_string($value) ? "'$value'" : $value;
+        $this->client->executeScript(
+            "$('$selector').val($val);"
+        );
+    }
+
+    protected function click(string $selector, array $options = [])
+    {
+        $this->expectToExist($selector, $options);
+        $this->getClient()->getCrawler()->filter($selector)->click();
+    }
+
+    protected function expectToExist(string $selector, array $options = [])
+    {
+        $timeout = $options['timeout'] ?? self::DEFAULT_TIMEOUT;
+        try {
+            $this->getClient()->waitFor($selector, $timeout);
+        } catch (TimeoutException) {
+            $this->fail("Expected to find '$selector' within $timeout seconds");
+        }
+    }
+
+    protected function screenshot(?string $name = null)
+    {
+        $this->getClient()->takeScreenshot($name);
+    }
+
     protected function login(string $username = TU_USER, string $password = TU_PASS)
     {
-        $this->getClient()->request('GET', '/');
-        $this->getClient()->waitFor('#login_name', 5);
-        $login_name = $this->getClient()->getCrawler()->filter('#login_name')->attr('name');
-        $password_name = $this->getClient()->getCrawler()->filter('input[type="password"]')->attr('name');
-        $this->getClient()->getCrawler()->selectButton('submit')->form()->get($login_name)->setValue($username);
-        $this->getClient()->getCrawler()->selectButton('submit')->form()->get($password_name)->setValue($password);
-        $this->getClient()->getCrawler()->selectButton('submit')->form()->get('auth')->setValue('local');
-        $this->getClient()->takeScreenshot('prelogin.png');
-        $this->getClient()->getCrawler()->filter('button[name="submit"]')->click();
-        $this->getClient()->takeScreenshot('postlogin.png');
+        //$this->getClient()->request('GET', '/');
+        $this->visit('/');
+        self::assertStringContainsString('Authentication', $this->title());
+
+        //$this->getClient()->waitFor('#login_name', 5);
+        //$login_name = $this->getClient()->getCrawler()->filter('#login_name')->attr('name');
+        //$password_name = $this->getClient()->getCrawler()->filter('input[type="password"]')->attr('name');
+        //$this->getClient()->getCrawler()->selectButton('submit')->form()->get($login_name)->setValue($username);
+        //$this->getClient()->getCrawler()->selectButton('submit')->form()->get($password_name)->setValue($password);
+        //$this->getClient()->getCrawler()->selectButton('submit')->form()->get('auth')->setValue('local');
+        $this->setValue('#login_name', $username);
+        $this->setValue('input[type="password"]', $password);
+        $this->setValue('select[name="auth"]', 'local');
+
+        //$this->getClient()->takeScreenshot('prelogin.png');
+        //$this->getClient()->getCrawler()->filter('button[name="submit"]')->click();
+        $this->screenshot('prelogin.png');
+        $this->click('button[name="submit"]');
+
+        //$this->getClient()->takeScreenshot('postlogin.png');
+        $this->screenshot('postlogin.png');
 
         // Expect to be taken to front/central.php
-        $this->getClient()->waitFor('div.page', 5);
-        $this->assertStringContainsString('central.php', $this->getClient()->getCurrentURL());
+        //$this->getClient()->waitFor('div.page', 5);
+        $this->expectToExist('div.page');
+        //$this->assertStringContainsString('central.php', $this->getClient()->getCurrentURL());
+        $this->assertStringContainsString('Standard', $this->title());
+        $this->assertStringContainsString('central.php', $this->url());
     }
 }
