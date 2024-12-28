383196450ec0f68aa59e470828864592ebd6517c
Curtis Conard <cconard96@gmail.com>
2024-12-28 16:43:32 -0500

    add test

---
+++ tests/cypress/e2e/dashboards.cy.js
@@ -0,0 +1,54 @@
+/**
+ * ---------------------------------------------------------------------
+ *
+ * GLPI - Gestionnaire Libre de Parc Informatique
+ *
+ * http://glpi-project.org
+ *
+ * @copyright 2015-2024 Teclib' and contributors.
+ * @copyright 2003-2014 by the INDEPNET Development Team.
+ * @licence   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * ---------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------------------------------------
+ */
+describe('Entities selector', () => {
+    beforeEach(() => {
+        cy.login();
+    });
+
+    const dashboards = new Map([
+        ['Asset', '/front/dashboard_assets.php'],
+        ['Assistance', '/front/dashboard_helpdesk.php'],
+        ['Central', '/front/central.php'],
+    ]);
+
+    dashboards.forEach((value, key) => {
+        it(`${key} Dashboard Loads`, () => {
+            cy.visit(value);
+            cy.get('.grid-stack-item .g-chart').should('be.visible');
+            // grid-stack-items should have reasonable height
+            cy.get('.grid-stack-item').each(($el) => {
+                cy.get($el).invoke('height').should('be.gt', 30);
+            });
+        });
+    });
+});
