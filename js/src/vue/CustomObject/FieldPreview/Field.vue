63b3a36c13bf91a7d48a62741ca996c9d6adbe97
Curtis Conard <cconard96@gmail.com>
2025-01-06 08:37:28 -0500

    drag full field

--- js/src/vue/CustomObject/FieldPreview/Field.vue
+++ js/src/vue/CustomObject/FieldPreview/Field.vue
@@ -17,14 +17,11 @@
 </script>
 
 <template>
-    <div :class="`sortable-field align-items-center ${field_options.full_width ? 'col-12' : 'col-12 col-sm-6'}`"
+    <div :class="`sortable-field align-items-center ${field_options.full_width ? 'col-12' : 'col-12 col-sm-6'} cursor-grab`"
          :data-key="field_key" :data-customfield-id="customfields_id">
         <input type="hidden" name="fields_display[]" :value="field_key" :disabled="!is_active" />
         <slot name="field_options"></slot>
         <div :class="`form-field row flex-grow-1 m-1`">
-            <div class="col-auto align-content-center">
-                <i class="ti ti-grip-vertical sort-handle"></i>
-            </div>
             <div class="col py-2">
                 <slot name="field_markers"></slot>
                 <slot name="field_label"></slot>
